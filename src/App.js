ff52d3749ccc1447218eb061e34f9ff03daf0091
itsrahulhere <rahulofspv@gmail.com>
2020-08-07 17:00:32 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo.png';
+import toggle from './assets/toggle.png';
+import play from './assets/play.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       <header>
+       <a href="#" class="logo"><img src={logo}  /></a>
+       <div class="toggle"><img src={toggle}  /></div>
+       </header>
+      <div class="banner">
+        <div class="content">
+          <h2>Put on a <span>Happy</span> face</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Lorem Ipsum is simply dummy text of the printing and typesetti
+        ng industry. Lorem Ipsum has been the industry's standard dummy text ever sinc
+        e the 1500s, when an unknown printer took a galley of type and scrambled it to 
+        make a type specimen book. It has survived nf Lorem Ipsum.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <a href="#" class="play" onClick="toggle();" ><img src={play} />Watch Trailer</a>
+        <div class="slide"></div>
+        <ul class="sci">
+          <li><a href="#"><img src={facebook} /></a></li>
+          <li><a href="#"><img src={twitter} /></a></li>
+          <li><a href="#"><img src={instagram} /></a></li>
+        </ul>
+
+        </div>
+      </div>
+      <div class="trailer">
+        <video src="trailer.mp4" controls="true"></video>
+        <img src="close" class="close " onClick="toggle();" />
+      </div>
+
+        
     </div>
   );
 }
+function toggle(){
+            var trailer = document.querySelector('.trailer');
+            var video = document.querySelector('video');
+            trailer.classList.toggle('active');
+            video.currentTime = 0;
+            video.pause();
+     }
 
 export default App;
